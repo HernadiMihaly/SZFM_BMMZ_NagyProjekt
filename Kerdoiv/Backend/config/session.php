31cbc5594306b34aa35e2ce7a5d3881c8bf7370f
SylwerStone <kissmarcell98@gmail.com>
2021-11-14 18:53:47 +0100

    Kérdőív Backend: session.php további bővítése

--- Kerdoiv/Backend/config/session.php
+++ Kerdoiv/Backend/config/session.php
@@ -99,3 +99,34 @@ return [
             | Affects: "apc", "dynamodb", "memcached", "redis"
             |
             */
+
+                'store' => env('SESSION_STORE', null),
+
+                /*
+                |--------------------------------------------------------------------------
+                | Session Sweeping Lottery
+                |--------------------------------------------------------------------------
+                |
+                | Some session drivers must manually sweep their storage location to get
+                | rid of old sessions from storage. Here are the chances that it will
+                | happen on a given request. By default, the odds are 2 out of 100.
+                |
+                */
+
+                'lottery' => [2, 100],
+
+                /*
+                |--------------------------------------------------------------------------
+                | Session Cookie Name
+                |--------------------------------------------------------------------------
+                |
+                | Here you may change the name of the cookie used to identify a session
+                | instance by ID. The name specified here will get used every time a
+                | new session cookie is created by the framework for every driver.
+                |
+                */
+
+                'cookie' => env(
+                    'SESSION_COOKIE',
+                    Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+                ),
