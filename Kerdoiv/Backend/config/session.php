716b78ac2c00dbe1695166556498fb2fe23d0a7b
SylwerStone <kissmarcell98@gmail.com>
2021-11-14 18:54:41 +0100

    Kérdőív Backend: session.php kiegészítése

--- Kerdoiv/Backend/config/session.php
+++ Kerdoiv/Backend/config/session.php
@@ -130,3 +130,42 @@ return [
                     'SESSION_COOKIE',
                     Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
                 ),
+
+                 /*
+                    |--------------------------------------------------------------------------
+                    | Session Cookie Path
+                    |--------------------------------------------------------------------------
+                    |
+                    | The session cookie path determines the path for which the cookie will
+                    | be regarded as available. Typically, this will be the root path of
+                    | your application but you are free to change this when necessary.
+                    |
+                    */
+
+                    'path' => '/',
+
+                    /*
+                    |--------------------------------------------------------------------------
+                    | Session Cookie Domain
+                    |--------------------------------------------------------------------------
+                    |
+                    | Here you may change the domain of the cookie used to identify a session
+                    | in your application. This will determine which domains the cookie is
+                    | available to in your application. A sensible default has been set.
+                    |
+                    */
+
+                    'domain' => env('SESSION_DOMAIN', null),
+
+                    /*
+                    |--------------------------------------------------------------------------
+                    | HTTPS Only Cookies
+                    |--------------------------------------------------------------------------
+                    |
+                    | By setting this option to true, session cookies will only be sent back
+                    | to the server if the browser has a HTTPS connection. This will keep
+                    | the cookie from being sent to you when it can't be done securely.
+                    |
+                    */
+
+                    'secure' => env('SESSION_SECURE_COOKIE'),
