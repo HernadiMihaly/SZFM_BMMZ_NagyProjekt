d6fae5386bac276bf853519dea98caae07a33fd8
Zsombor Osztós <osztoszsombor6@gmail.com>
2021-11-14 16:31:54 +0100

    Kérdőív Frontend Kerdőívek komponens elkészítése

---
+++ Kerdoiv/Frontend/src/components/Kerdoivek.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>Kitölthető kérdőívek</h1>
+    <ul class="kerdoivek">
+        <li class="kerdoiv" v-for="kerdoiv in kerdoivek" v-bind:key="kerdoiv.id">
+          <router-link :to="{ name: 'kerdoiv', params: { kerdoiv_id: kerdoiv.id } }">{{ kerdoiv.title }}</router-link>
+          <Button icon="pi pi-user-edit" class="p-button-rounded p-button-primary" @click="edit_kerdoiv(kerdoiv.id)"/>
+          <Button icon="pi pi-trash" class="p-button-rounded p-button-danger" @click="delete_kerdoiv(kerdoiv.id)"/>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Button from 'primevue/button';
+export default {
+  name: 'Kerdoivek',
+  props: {
+    msg: String
+  },
+  components: {
+    Button
+  },
+  data () {
+    return {
+      info: null,
+      kerdoivek: []
+    }
+  },
+  mounted () {
+    this.axios
+      .get('http://localhost:8000/api/kerdoivek')
+      .then(response => {
+        this.info = response.data;
+        //var r = JSON.parse(response.data);
+        this.kerdoivek = response.data;
+        //this.info = JSON.stringify(this.kerdoivek);
+      })
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+
