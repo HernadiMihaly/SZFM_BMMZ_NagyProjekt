7837759a097cd2738405d7200f0b60925a70db59
SylwerStone <kissmarcell98@gmail.com>
2021-11-14 18:41:58 +0100

    Kérdőív Backend: logging.php bővítése

--- Kerdoiv/Backend/config/logging.php
+++ Kerdoiv/Backend/config/logging.php
@@ -31,3 +31,18 @@ return [
         */
 
         'deprecations' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
+
+     /*
+         |--------------------------------------------------------------------------
+         | Log Channels
+         |--------------------------------------------------------------------------
+         |
+         | Here you may configure the log channels for your application. Out of
+         | the box, Laravel uses the Monolog PHP logging library. This gives
+         | you a variety of powerful log handlers / formatters to utilize.
+         |
+         | Available Drivers: "single", "daily", "slack", "syslog",
+         |                    "errorlog", "monolog",
+         |                    "custom", "stack"
+         |
+         */
