213666f10fe7e42cb9bc475a575ae03add4eb9fb
SylwerStone <kissmarcell98@gmail.com>
2021-11-14 18:51:05 +0100

    Kérdőív Backend: sanctum.php befejezve

--- Kerdoiv/Backend/config/sanctum.php
+++ Kerdoiv/Backend/config/sanctum.php
@@ -29,3 +29,23 @@ return [
     | not expire. This won't tweak the lifetime of first-party sessions.
     |
     */
+
+    'expiration' => null,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Sanctum Middleware
+        |--------------------------------------------------------------------------
+        |
+        | When authenticating your first-party SPA with Sanctum you may need to
+        | customize some of the middleware Sanctum uses while processing the
+        | request. You may change the middleware listed below as required.
+        |
+        */
+
+        'middleware' => [
+            'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+            'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
+        ],
+
+    ];
