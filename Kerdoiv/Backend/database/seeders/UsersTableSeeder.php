c5140c2f35ad6bdc28a243ec6d7416a5109979df
BalazsBence <43274435+BalazsBence@users.noreply.github.com>
2021-11-15 11:55:43 +0100

    Kérdőív Backend finsihed UsersTableSeeder

--- Kerdoiv/Backend/database/seeders/UsersTableSeeder.php
+++ Kerdoiv/Backend/database/seeders/UsersTableSeeder.php
@@ -1,1 +1,42 @@
+<?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use \App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        // Let's make sure everyone has the same password and 
+        // let's hash it before the loop, or else our seeder 
+        // will be too slow.
+        $password = Hash::make('kerdoiv');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password,
+			'is_admin' => true
+        ]);
+
+        // And now let's generate a few dozen users for our app:
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+				'is_admin' => false
+            ]);
+        }
+    }
+}
