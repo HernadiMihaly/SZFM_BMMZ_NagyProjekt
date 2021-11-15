693afcbfeb86c6f539591640b6e210c8ccbbe94c
BalazsBence <43274435+BalazsBence@users.noreply.github.com>
2021-11-15 12:04:49 +0100

    Kérdőív Frontend Finsihed main.js

--- Kerdoiv/Frontend/src/components/main.js
+++ Kerdoiv/Frontend/src/components/main.js
@@ -1,1 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import router from './router'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/primevue.min.css'
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css'
+import 'primeicons/primeicons.css'
 
+const app = createApp(App).use(router)
+app.use(VueAxios, axios)
+app.use(PrimeVue);
+app.config.globalProperties.axios=axios
+app.mount('#app')
