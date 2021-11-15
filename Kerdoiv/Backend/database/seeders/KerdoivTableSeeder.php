54b12a0f02840e92b4380b42cbe423955514924e
BalazsBence <43274435+BalazsBence@users.noreply.github.com>
2021-11-15 11:54:09 +0100

    Kérdőív Backend Finished KerdoivTableSeeder

--- Kerdoiv/Backend/database/seeders/KerdoivTableSeeder.php
+++ Kerdoiv/Backend/database/seeders/KerdoivTableSeeder.php
@@ -1,1 +1,34 @@
+<?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use \App\Models\Kerdoiv;
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+class KerdoivTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 5; $i++) {
+            Kerdoiv::create([
+                'title' => $faker->sentence,
+				'is_separated' => $faker->boolean,
+				'is_randomised' => $faker->boolean
+            ])->each(function ($kerdoiv) {
+				$kerdesek = \App\Models\Kerdes::factory()->count(10)->create();
+				$kerdesek->each(function ($kerdes){
+					$kerdes->kerdoiv_id = NULL;
+				});
+				$kerdoiv->kerdesek()->saveMany($kerdesek);
+			});
+        }
+    }
+}
