ce000d4065679cad1917b1e4ea2264329bc22217
misi1201 <szilagyimihaly2000@gmail.com>
2021-11-28 18:57:40 +0100

    Kérdőív Frontend: Login.vue hozzáadása a projekthez

---
+++ Kerdoiv/Frontend/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is a login page</h1>
+  </div>
+  <div>
+    <LoginForm/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'Login',
+  components: {
+     LoginForm
+  }
+}
+</script>
