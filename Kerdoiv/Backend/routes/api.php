b390facacc912ff7eafdace4c7adb2e84f608ef7
Zsombor Osztós <osztoszsombor6@gmail.com>
2021-11-28 10:40:48 +0100

    Kérdőív Backend route-okhoz autorizáció beállítása

--- Kerdoiv/Backend/routes/api.php
+++ Kerdoiv/Backend/routes/api.php
@@ -22,8 +22,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('kerdoivek', 'App\Http\Controllers\KerdoivController@index');
 //Route::middleware('auth:sanctum')->get('kerdoivek/{kerdoiv}', 'App\Http\Controllers\KerdoivController@show');
 Route::get('kerdoivek/{kerdoiv}', 'App\Http\Controllers\KerdoivController@show');
-Route::post('kerdoivek', 'App\Http\Controllers\KerdoivController@store');
-Route::put('kerdoivek/{kerdoiv}', 'App\Http\Controllers\KerdoivController@update');
-Route::delete('kerdoivek/{kerdoiv}', 'App\Http\Controllers\KerdoivController@delete');
+Route::middleware('auth:sanctum')->post('kerdoivek', 'App\Http\Controllers\KerdoivController@store');
+Route::middleware('auth:sanctum')->put('kerdoivek/{kerdoiv}', 'App\Http\Controllers\KerdoivController@update');
+Route::middleware('auth:sanctum')->delete('kerdoivek/{kerdoiv}', 'App\Http\Controllers\KerdoivController@delete');
+
+Route::post('login', 'App\Http\Controllers\LoginController@login');
+Route::post('register', 'App\Http\Controllers\LoginController@register');
 
 Route::post('valaszokmentese', 'App\Http\Controllers\ValaszController@store');
+Route::/*middleware('auth:sanctum')->*/get('valaszok/{kerdoiv}', 'App\Http\Controllers\ValaszController@list');
