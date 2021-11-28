8153a6489d6c866a7fc0eb108616648e5eee4698
Zsombor Osztós <osztoszsombor6@gmail.com>
2021-11-28 10:42:33 +0100

    Kérdőív Frontend Válaszlistázás nézet megadása

---
+++ Kerdoiv/Frontend/src/views/ListAnswers.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="list">
+    <ValaszLista/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ValaszLista from '@/components/ValaszLista.vue'
+
+export default {
+  name: 'ListAnswers',
+  components: {
+    ValaszLista
+  }
+}
+</script>
