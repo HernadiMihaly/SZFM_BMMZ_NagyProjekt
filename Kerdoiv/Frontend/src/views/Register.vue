176f164a7a5ba2aabbdfb7563073d10c409f3e38
SylwerStone <43640947+SylwerStone@users.noreply.github.com>
2021-11-28 17:52:44 +0100

    Register feltöltve

---
+++ Kerdoiv/Frontend/src/views/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>Új Felhasználó regisztrálása</h1>
+  </div>
+  <div>
+    <RegisterForm/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RegisterForm from '@/components/RegisterForm.vue'
+
+export default {
+  name: 'Register',
+  components: {
+     RegisterForm
+  }
+}
+</script>
